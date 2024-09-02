517980139d3135c7355b1466710872e67ab86eea
Igor Carvalho <113865320+IgorCarvalhoz@users.noreply.github.com>
2024-09-02 10:51:22 +0000

    leetcode dia 1

--- teste.js
+++ teste.js
@@ -1,1 +1,32 @@
-console.log("Running")
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var romanToInt = function(s) {
+    const map = new Map();
+    map.set("I", 1);
+    map.set("V", 5);
+    map.set("X", 10);
+    map.set("L", 50);
+    map.set("C", 100);
+    map.set("D", 500);
+    map.set("M", 1000);
+    
+    let total = 0;
+    let values = s.split("");
+
+    for (let i = 0; i < values.length; i++) {
+        let currentValue = map.get(values[i]);
+        let nextValue = map.get(values[i + 1]);
+
+        if (nextValue && currentValue < nextValue) {
+            total -= currentValue;
+        } else {
+            total += currentValue;
+        }
+    }
+    
+    return total;
+};
+
+console.log(romanToInt("LV"));
